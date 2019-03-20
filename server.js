52e0635d1faafa6f10a9b4fbea79809ee89ebbab
Aman <amsingh714@gmail.com>
2019-03-20 13:18:34 -0400

    added helmet

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require("helmet");
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// middleware
 server.use(express.json());
+server.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP headers. 
 
+
+// routing
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res, next) => {
